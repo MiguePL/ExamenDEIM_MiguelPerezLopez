e9a2e5bb0be6ce3e1036a78190b7bb34b3a2bc67
MiguePL <miguepl95@gmail.com>
2016-12-20 20:16:36 +0100

    #10 Nave

---
+++ Assets/Scripts/Nave.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nave : MonoBehaviour {
+	private Rigidbody2D rb;
+	public float power = 1f;
+	public float velocidad = 5f;
+	public float salto = 400;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("right")) {
+			rb.velocity = new Vector2 (velocidad * power, rb.velocity.y);
+		}
+		if (Input.GetKeyDown ("left")) {
+			rb.velocity = new Vector2 (-velocidad * power, rb.velocity.y);
+
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rb.AddForce (new Vector2 (0, salto));
+		}
+	
+	}
+}
