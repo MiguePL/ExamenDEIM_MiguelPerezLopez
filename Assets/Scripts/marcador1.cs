e0759d78d4523721da4eb50b6ef17c8f6a6d7572
MiguePL <miguepl95@gmail.com>
2016-12-20 20:12:01 +0100

    #9 Escenario Nave

---
+++ Assets/Scripts/marcador1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class marcador1 : MonoBehaviour {
+
+	private int goles = 0;
+	Text texto; 
+
+	void Start(){
+		texto = GetComponent<Text> ();
+	}
+
+	public void suma_goles(int cantidad){
+		goles = goles + cantidad; //monedas += cantidad;
+		if (goles < 0) {
+			goles = 0;
+		}
+		texto.text = goles.ToString ();
+	}
+
+	public void resetear(){
+		goles = 0;
+		texto.text = goles.ToString ();
+	}
+}
